9e348d5de766424aae775ed54d54573850faa5b6
Rifa9oct <kohinuraktherr@gmail.com>
2023-08-19 18:41:07 +0600

    append li in area calculation field

--- js/calculation.js
+++ js/calculation.js
@@ -3,7 +3,8 @@ function calculateTriangleArea(){
     const base = getInputFieldValue('base1');
     const height = getInputFieldValue('height1');
     const area = 0.5 * base * height;
-    setElementText('triangle-area', area); 
+    setElementText('triangle-area', area);
+    appendLi('Triangle',area); 
 } 
 
 // Rectangle
@@ -12,6 +13,7 @@ function calculateRectangleArea(){
     const length = getInputFieldValue('length');
     const area = width * length;
     setElementText('rectangle-area', area); 
+    appendLi('Rectangle',area); 
 } 
 
 // Parallelogram
@@ -20,6 +22,7 @@ function calculateParallelogramArea(){
     const height = getInputFieldValue('height2');
     const area = base * height;
     setElementText('parallelogram-area', area); 
+    appendLi('Parallelogram',area); 
 }
 
 // Ellipse
@@ -27,17 +30,25 @@ function calculateEllipseArea(){
     const a = getInputFieldValue('a');
     const b = getInputFieldValue('b');
     const area = 3.1416 * a * b;
-    setElementText('ellipse-area', area); 
+    setElementText('ellipse-area', area);
+    appendLi('Ellipse',area);  
 } 
 
 function getInputFieldValue(elementId){
     const element = document.getElementById(elementId);
     const elementValueString = element.value;
     const elementValue = parseFloat(elementValueString);
+    element.value = '';
     return elementValue;
 }
 
 function  setElementText(setId,setValue){
     const setArea = document.getElementById(setId);
     setArea.innerText = setValue;
+}
+
+function appendLi(name,value){
+    const li = document.createElement('li');
+    li.innerText = name + ' ' + 'Area' + ' = ' + value + " cm";
+    document.getElementById('areaContainer').appendChild(li);
 }
